849f676c21becfca908a32ee260a6f20a13209ff
Ilian Kukov <iliankukov@gmail.com>
2023-01-24 19:35:37 +0200

    HW 24.01.23

---
+++ src/_11FuncionalProgramming/_01SortEvenNumbers.java
@@ -0,0 +1,14 @@
+package _11FuncionalProgramming;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _01SortEvenNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        Arrays.stream(scanner.next().split(", "))
+                .fi
+    }
+}
